43a629696482d78b9be60bdd171637eda0c82eaa
Edwin Török <edvin.torok@citrix.com>
2018-07-27 13:57:05 +0000

    CP-28659: plumb through nvram parameter
    Signed-off-by: Edwin Török <edvin.torok@citrix.com>

--- src/xn.ml
+++ src/xn.ml
@@ -409,6 +409,7 @@ let add' copts x () = match x with
 						qemu_disk_cmdline = false;
 						qemu_stubdom = false;
 						firmware = None;
+						nvram = None;
 					} in
 				let uuid = if mem _uuid then find _uuid |> string else Uuidm.to_string (Uuidm.create `V4) in
 				let name = if mem _name then find _name |> string else uuid in
